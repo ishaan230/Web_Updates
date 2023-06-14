3f2b0a39470c75a6e919293eae8a7231f02d434c
Ishaan Sharma <ishaansharma1975@gmail.com>
2023-06-15 01:55:20 +0530

    Added Cpu Updates

--- cpu_updates.py
+++ cpu_updates.py
@@ -21,24 +21,15 @@ def send_discord_webhook(webhook_url, message):
 # Replace 'YOUR_WEBHOOK_URL' with your actual Discord webhook URL
 webhook_url = ''
 while True:
-    if psutil.sensors_temperatures():
-        a = psutil.sensors_temperatures()
+    if psutil.cpu_freq():
+        a = psutil.cpu_freq()
         print(a)
-        # exit()
-        cputemp_str = a['k10temp'][0] 
-        cpu_current = cputemp_str[1]
-        cpu_peak = cputemp_str[2]
-
-        disk_str = a['nvme'][0]
-        disk_current = disk_str[1]
-        disk_peak = disk_str[2]
-
-        gpu_str = a['amdgpu'][0]
-        gpu_current = gpu_str[1]
-        gpu_peak = gpu_str[2]
-
-        message = f"Current CPU Temp : {cpu_current}°C\n Highest Temp : {cpu_peak}°C\n\n Current Disk Temp : {disk_current}°C\n Peak Disk : {disk_peak}°C\n\n Current Gpu Temp : {gpu_current}°C\nPeak Gpu Temp : {gpu_peak}°C"
+        current_cpu = round(a[0],2)
+        min_cpu = round(a[1]/1000,2)
+        max_cpu = round(a[2]/1000,2)
+        message = f"\nCurrent Cpu Usage: {current_cpu}Ghz\nMin Cpu Usage : {min_cpu}Ghz\nMax Cpu Usage : {max_cpu}Ghz"
         send_discord_webhook(webhook_url, message)
         time.sleep(300)
     else:
-        print('Error Connecting to PC!')
+        print('Error connecting to PC!')
+
