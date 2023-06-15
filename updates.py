a32234ced9a2d69fa14f780d668c39e4daef2ea0
Ishaan Sharma <ishaansharma1975@gmail.com>
2023-06-15 11:40:15 +0530

    Optimised

--- updates.py
+++ updates.py
@@ -3,7 +3,7 @@ import requests
 import time
 import json
 
-def send_discord_webhook(webhook_url, message):
+def send_discord_webhook_battery(message):
     payload = {
         'content': message
     }
@@ -12,14 +12,49 @@ def send_discord_webhook(webhook_url, message):
         'Content-Type': 'application/json'
     }
 
-    response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+    webhook_url_battery = ''
+
+    response = requests.post(webhook_url_battery, data=json.dumps(payload), headers=headers)
     if response.status_code == 204:
         print("Message sent successfully to Discord!")
     else:
         print("Failed to send message to Discord. Status code:", response.status_code)
 
-# Replace 'YOUR_WEBHOOK_URL' with your actual Discord webhook URL
-webhook_url = ''
+def send_discord_webhook_temp(message):
+    payload = {
+        'content': message
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    webhook_url_temp = ''
+
+    response = requests.post(webhook_url_temp, data=json.dumps(payload), headers=headers)
+    if response.status_code == 204:
+        print("Message sent successfully to Discord!")
+    else:
+        print("Failed to send message to Discord. Status code:", response.status_code)
+
+
+def send_discord_webhook_cpu(message):
+    payload = {
+        'content': message
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    webhook_url_cpu = ''
+
+    response = requests.post(webhook_url_cpu, data=json.dumps(payload), headers=headers)
+    if response.status_code == 204:
+        print("Message sent successfully to Discord!")
+    else:
+        print("Failed to send message to Discord. Status code:", response.status_code)
+
 while True:
 # Check if the PC is awake
     if psutil.CONN_ESTABLISHED == 'ESTABLISHED':
@@ -27,15 +62,23 @@ while True:
         # For Battery Info
         battery_info = psutil.sensors_battery()
         message = f"Battery : {battery_info[0]}\n Power Plugged : {battery_info[2]}"
-        send_discord_webhook(webhook_url, message)
+        send_discord_webhook_battery(message)
         
+
+
+
+
         # For CPU Info
         cpu_info = psutil.cpu_freq()
         current_cpu = round(cpu_info[0],2)
         min_cpu = round(cpu_info[1]/1000,2)
         max_cpu = round(cpu_info[2]/1000,2)
         message = f"\nCurrent Cpu Usage: {current_cpu}Ghz\nMin Cpu Usage : {min_cpu}Ghz\nMax Cpu Usage : {max_cpu}Ghz"
-    
+        send_discord_webhook_cpu(message)
+
+
+
+
         #For Temp Info 
         a = psutil.sensors_temperatures()
         # print(a)
@@ -52,10 +95,17 @@ while True:
         gpu_current = gpu_str[1]
         gpu_peak = gpu_str[2]
         
+        message = f"\nCurrent CPU Temp : {cpu_current}°C\n Highest Temp : {cpu_peak}°C\n\n Current Disk Temp : {disk_current}°C\n Peak Disk : {disk_peak}°C\n\n Current Gpu Temp : {gpu_current}°C\nPeak Gpu Temp : {gpu_peak}°C"
+        send_discord_webhook_temp(message)
+
+
+
+        
         #send message
-        message = f"Battery : {battery_info[0]}\n Power Plugged : {battery_info[2]}\n\nCurrent Cpu Usage: {current_cpu}Ghz\nMin Cpu Usage : {min_cpu}Ghz\nMax Cpu Usage : {max_cpu}Ghz\n\nCurrent CPU Temp : {cpu_current}°C\n Highest Temp : {cpu_peak}°C\n\n Current Disk Temp : {disk_current}°C\n Peak Disk : {disk_peak}°C\n\n Current Gpu Temp : {gpu_current}°C\nPeak Gpu Temp : {gpu_peak}°C"
-        send_discord_webhook(webhook_url, message)
+        # message = f"Battery : {battery_info[0]}\n Power Plugged : {battery_info[2]}\n\nCurrent Cpu Usage: {current_cpu}Ghz\nMin Cpu Usage : {min_cpu}Ghz\nMax Cpu Usage : {max_cpu}Ghz\n\nCurrent CPU Temp : {cpu_current}°C\n Highest Temp : {cpu_peak}°C\n\n Current Disk Temp : {disk_current}°C\n Peak Disk : {disk_peak}°C\n\n Current Gpu Temp : {gpu_current}°C\nPeak Gpu Temp : {gpu_peak}°C"
+        # send_discord_webhook(webhook_url, message)
 
         time.sleep(300)
     else:
         print('Error connecting to PC!')
+        exit()
